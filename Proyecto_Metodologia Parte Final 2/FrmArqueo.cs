aeb288973794f843e08ab04dd03c10fce9ac1589
Alexander Javier Huaman Guevara <79520917+CALICHIN555@users.noreply.github.com>
2021-03-04 08:03:02 -0500

    Add files via upload

---
+++ Proyecto_Metodologia Parte Final 2/FrmArqueo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Proyecto_Metodologia
+{
+    public partial class FrmArqueo : Form
+    {
+        private DataSet aDatos;
+        public FrmArqueo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConteo_Click(object sender, EventArgs e)
+        {
+            FrmConteo c = new FrmConteo();
+            c.ShowDialog();
+            float b = c.obtenerdatos();
+            if (b != 0)
+            {
+                txtconteo.Text = b.ToString();
+            }
+        }
+        public DataSet Datos
+        {
+            get { return aDatos; }
+        }
+        public DataSet EjecutarSelect(string pConsulta)
+        {//-- Método para ejecutar consultas del tipo SELECT
+
+            using (SqlConnection conexion = new SqlConnection("Data Source=localhost;" +
+               "Initial Catalog=BDSISTEMA_VENTAS;Integrated Security=SSPI;"))
+            {
+                conexion.Open();
+                SqlDataAdapter a = new SqlDataAdapter();
+                using (SqlCommand cmd = new SqlCommand(pConsulta, conexion)) ;
+                a.SelectCommand = new SqlCommand(pConsulta, conexion);
+                aDatos = new DataSet();
+                // aAdaptador.Fill(aDatos);
+                a.Fill(aDatos);
+                conexion.Close();
+            }
+            return aDatos;
+        }
+        public string ValorAtributo(string pNombreCampo)
+        {//-- Recupera el valor de un atributo del dataset
+            if (Datos.Tables[0].Rows.Count > 0)
+            {
+                return Datos.Tables[0].Rows[0][pNombreCampo].ToString();
+            }
+            else
+                return "";
+        }
+        private void dateTimeArqueo_ValueChanged(object sender, EventArgs e)
+        {
+           dgventas.DataSource= validarfecha();
+            dgventas.Columns["CodVenta"].Width = 150;
+            dgventas.Columns["CodVenta"].HeaderText = "Codigo de Venta";
+            dgventas.Columns["PrecioTotal"].Width = 160;
+            dgventas.Columns["Fecha"].Width = 180;
+            total();
+        }
+        public DataTable validarfecha()
+        {
+            string Consulta = "SELECT * FROM TVentas WHERE Fecha = '" +dateTimeArqueo.Value.ToShortDateString()  + "'";
+          
+            EjecutarSelect(Consulta);
+            return Datos.Tables[0];
+        }
+        public void total()
+        {
+            int i = 0;
+            float total = 0;
+            float sub = 0;
+            while (i<dgventas.RowCount)
+            {
+                sub = float.Parse(dgventas[1, i].Value.ToString());
+                total = total + sub;
+                i++;
+            }
+            txttotal.Text = total.ToString();
+        }
+
+        private void txtconteo_TextChanged(object sender, EventArgs e)
+        {
+            if(txttotal.Text!="" && txtconteo.Text != "")
+            {
+                lbDiferencia.Text = (double.Parse(txttotal.Text) - double.Parse(txtconteo.Text)).ToString();
+            }
+        }
+    }
+}
---
+++ Proyecto_Metodologia Parte Final 2/FrmOlvidasteContraseña.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Proyecto_Metodologia
+{
+    partial class FrmOlvidasteContraseña
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmOlvidasteContraseña));
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonIngresar = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.PowerOff;
+            this.iconButton1.IconColor = System.Drawing.Color.White;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 30;
+            this.iconButton1.Location = new System.Drawing.Point(717, 18);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(52, 34);
+            this.iconButton1.TabIndex = 19;
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(154, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(321, 32);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Recuperar Contraseña";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.labelUsuario);
+            this.panel3.Controls.Add(this.txtCorreo);
+            this.panel3.Location = new System.Drawing.Point(154, 104);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(497, 84);
+            this.panel3.TabIndex = 17;
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.labelUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsuario.ForeColor = System.Drawing.Color.White;
+            this.labelUsuario.Location = new System.Drawing.Point(0, 17);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(68, 24);
+            this.labelUsuario.TabIndex = 5;
+            this.labelUsuario.Text = "Correo";
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCorreo.Location = new System.Drawing.Point(74, 17);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(401, 29);
+            this.txtCorreo.TabIndex = 0;
+            this.txtCorreo.TextChanged += new System.EventHandler(this.txtusuario_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(250, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 32);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Supermercado Unsaac";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel2.Location = new System.Drawing.Point(657, 58);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(131, 175);
+            this.panel2.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(136, 270);
+            this.panel1.TabIndex = 13;
+            // 
+            // buttonIngresar
+            // 
+            this.buttonIngresar.BackColor = System.Drawing.Color.Azure;
+            this.buttonIngresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonIngresar.Font = new System.Drawing.Font("Palatino Linotype", 14.16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIngresar.ForeColor = System.Drawing.Color.Black;
+            this.buttonIngresar.Location = new System.Drawing.Point(327, 212);
+            this.buttonIngresar.Name = "buttonIngresar";
+            this.buttonIngresar.Size = new System.Drawing.Size(172, 47);
+            this.buttonIngresar.TabIndex = 12;
+            this.buttonIngresar.Text = "Recuperar";
+            this.buttonIngresar.UseVisualStyleBackColor = false;
+            this.buttonIngresar.Click += new System.EventHandler(this.buttonIngresar_Click);
+            // 
+            // FrmOlvidasteContraseña
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 308);
+            this.Controls.Add(this.iconButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonIngresar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmOlvidasteContraseña";
+            this.Text = "FrmOlvidasteContraseña";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonIngresar;
+    }
+}
